01114ee8fd86b20d448ad2a78cb7f8d3c47e7253
Matt Radbourne <m@radbourne.me>
2016-07-17 17:15:55 +0100

    Added Gulp webserver

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,24 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var webserver = require('gulp-webserver');
 
 gulp.task('sass', function(){
-  return gulp.src('app/scss/styles.scss')
+  return gulp.src('app/scss/application.scss')
     .pipe(sass())
     .pipe(gulp.dest('app/css'));
 });
+
+gulp.task('webserver', function() {
+  gulp.src('app')
+    .pipe(webserver({
+      livereload: true,
+      directoryListing: false,
+      open: true
+    }));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('app/scss/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass', 'webserver', 'watch']);
